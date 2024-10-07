8d4a6a404ca6618fe3856c44ac3c77a78660ce7d
sudonorm <ayo.toba@gmail.com>
2024-10-06 21:28:34 -0600

    add sample items

--- db/push_data_to_db.py
+++ db/push_data_to_db.py
@@ -47,11 +47,107 @@ def main():
     ##### CHANGE THE PATHS!!!
     ################### For first time setup of static datasets, read in the datasets and upload ############################
 
-    FILE: str = r"/path/to/some_data.json"
+    # FILE: str = r"/path/to/some_data.json"
 
-    some_data: pd.DataFrame = pd.read_json(FILE)
+    # some_data: pd.DataFrame = pd.read_json(FILE)
 
-    upload.upload_some_data(df=some_data)
+    # upload.upload_some_data(df=some_data)
+
+    test_items = [
+        {
+            "user_id": 1,
+            "name": "Headphones",
+            "description": "This can be used to listen to soul lifting music",
+            "price": 99,
+            "slugs": "asdf4324/headphones",
+        },
+        {
+            "user_id": 1,
+            "name": "iPhone",
+            "description": "Access the world from your pocket",
+            "price": 599,
+            "slugs": "asdf4324/iphone",
+        },
+        {
+            "user_id": 1,
+            "name": "Batman Costume",
+            "description": "Great gear for important work",
+            "price": 79,
+            "slugs": "asdf4324/batman-costume",
+        },
+        {
+            "user_id": 2,
+            "name": "Headphones",
+            "description": "This can be used to listen to soul lifting music",
+            "price": 99,
+            "slugs": "asdf4324/headphones",
+        },
+        {
+            "user_id": 2,
+            "name": "iPhone",
+            "description": "Access the world from your pocket",
+            "price": 599,
+            "slugs": "asdf4324/iphone",
+        },
+        {
+            "user_id": 2,
+            "name": "Batman Costume",
+            "description": "Great gear for important work",
+            "price": 79,
+            "slugs": "asdf4324/batman-costume",
+        },
+        {
+            "user_id": 3,
+            "name": "Headphones",
+            "description": "This can be used to listen to soul lifting music",
+            "price": 99,
+            "slugs": "asdf4324/headphones",
+        },
+        {
+            "user_id": 3,
+            "name": "iPhone",
+            "description": "Access the world from your pocket",
+            "price": 599,
+            "slugs": "asdf4324/iphone",
+        },
+        {
+            "user_id": 3,
+            "name": "Batman Costume",
+            "description": "Great gear for important work",
+            "price": 79,
+            "slugs": "asdf4324/batman-costume",
+        },
+        {
+            "user_id": 4,
+            "name": "Headphones",
+            "description": "This can be used to listen to soul lifting music",
+            "price": 99,
+            "slugs": "asdf4324/headphones",
+        },
+        {
+            "user_id": 4,
+            "name": "iPhone",
+            "description": "Access the world from your pocket",
+            "price": 599,
+            "slugs": "asdf4324/iphone",
+        },
+        {
+            "user_id": 4,
+            "name": "Batman Costume",
+            "description": "Great gear for important work",
+            "price": 79,
+            "slugs": "asdf4324/batman-costume",
+        },
+    ]
+
+    items = pd.DataFrame(test_items)
+
+    upload.upload_any_more_than_three(
+        df=items,
+        dbTable_class="dataModel.UserItem",
+        dbTable_name="user_items",
+        pk_key_col="id",
+    )
 
     #########################################################################################
 
